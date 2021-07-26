a00db498f2835db7172641c78d9ff8b1552ce22e
zhangjizhao <zhangjizhao@canaan-creative.com>
2021-07-26 16:41:09 +0800

    more test

--- python/setup.py
+++ python/setup.py
@@ -3,12 +3,21 @@
 from distutils.core import setup
 import setuptools
 
+try:
+    from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+    class bdist_wheel(_bdist_wheel):
+        def finalize_options(self):
+            _bdist_wheel.finalize_options(self)
+            self.root_is_pure = False
+except ImportError:
+    bdist_wheel = None
+
 setup(
     name='warpctc-caffe',
     version='1.0.0',
     description='warpctc-caffe',
     packages=setuptools.find_packages(exclude=[]),
     package_data={'caffe': ['_caffe.so']},
-    platforms=['Linux']
+    cmdclass={'bdist_wheel': bdist_wheel}
 )
 
